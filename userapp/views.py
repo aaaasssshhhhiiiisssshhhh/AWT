9d960b88c13a814ab6a307be17c73c266e53ba41
aaaasssshhhhiiiisssshhhh <aashishkarki11@gmail.com>
2022-07-22 14:12:31 +0545

    some templates added

--- userapp/views.py
+++ userapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render (request,"login.html",{})
+
+def register(request):
+    return render (request,"register.html",{})
+
+def forgetpass(request):
+    return render( request ,"forgetpass.html",{})
+
+
 def home(request):
     return render(request,"home.html",{})
